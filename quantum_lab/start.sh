017f19a36013614af12cf6f77f8331361e52d0dd
Michaël <mika89700@gmail.com>
2021-06-02 14:10:08 +0200

    Update start.sh

--- quantum_lab/start.sh
+++ quantum_lab/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm start_jupyter.sh
+
 #openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout mykey.key -out mycert.pem
 
 #jupyter notebook --allow-root --port=8888 --no-browser --ip='*' --NotebookApp.token='' --NotebookApp.password='' --certfile=mycert.pem --keyfile mykey.key
