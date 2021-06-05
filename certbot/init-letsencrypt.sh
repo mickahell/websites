dcdd4f8438e97c7dd6cdb16aab451474e72c73a7
Michaël <mika89700@gmail.com>
2021-06-06 01:05:40 +0200

    Update init-letsencrypt.sh

--- certbot/init-letsencrypt.sh
+++ certbot/init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=(xtraorbitals.xyz www.xtraorbitals.xyz)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="michael.rollin@orange.fr" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -76,5 +76,4 @@ docker-compose run --rm --entrypoint "\
     --force-renewal" certbot
 echo
 
-echo "### Reloading nginx ..."
-docker-compose exec nginx nginx -s reload
+echo "### well done..."
