4f2623b3a82254624480a86a3f81a58f4cfebd9f
Michaël <mika89700@gmail.com>
2021-06-05 21:24:04 +0200

    Update start_jupyter.sh

--- quantum_lab/jupyter/start_jupyter.sh
+++ quantum_lab/jupyter/start_jupyter.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout mykey.key -out mycert.pem
-
-#jupyter notebook --allow-root --port=8888 --no-browser --ip='*' --NotebookApp.token='' --NotebookApp.password='' --certfile=mycert.pem --keyfile mykey.key
-
 cd /opt/quantum_lab/data/share
 
 jupyter notebook --allow-root --port=9999 --no-browser --ip='*' --NotebookApp.token='' --NotebookApp.password=''
