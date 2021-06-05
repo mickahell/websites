34574ea1c3429cc7fabce9f57b24ab427d7efa1b
Mica <mika89700@gmail.com>
2021-06-06 01:25:33 +0200

    main page

---
+++ main/app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(page_title="xtra Orbitals", page_icon=":space_invader:", layout='wide', initial_sidebar_state='auto')
+
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
+header = """
+<div align="center">
+    <h1>xtra Orbitals</h1>
+</div>
+<br /><br />
+"""
+st.markdown(header, unsafe_allow_html=True)
---
+++ main/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /opt/main
+streamlit run app.py
