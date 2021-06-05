ef38d0777ca716b1bfe221e50d9d804f591f120a
Michaël <mika89700@gmail.com>
2021-06-05 21:30:56 +0200

    Update start_jupyter.sh

--- quantum_lab/jupyter/start_jupyter.sh
+++ quantum_lab/jupyter/start_jupyter.sh
@@ -2,4 +2,15 @@
 
 cd /opt/quantum_lab/data/share
 
-jupyter notebook --allow-root --port=9999 --no-browser --ip='*' --NotebookApp.token='' --NotebookApp.password=''
+if [[ $LAB = "QISKIT" ]]
+then
+    port=9999
+elif [[ $LAB = "PENNYLANE" ]]
+then
+    port=9998
+elif [[ $LAB = "CIRQ" ]]
+then
+    port=9997
+fi
+
+jupyter notebook --allow-root --port=$port --no-browser --ip='*' --NotebookApp.token='' --NotebookApp.password=''
