e96d43da301f8b1acea4f647fa7ec9be6b4d7ca4
Mica <mika89700@gmail.com>
2021-06-06 13:40:29 +0200

    add bootstrap script

---
+++ bootstrap/server_install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Install system
+sudo apt-get install docker.io nginx git -y
+
+# Install certbot
+sudo snap install core; sudo snap refresh core
+sudo snap install --classic certbot
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
+sudo snap set certbot trust-plugin-with-root=ok
+
+# Install certificat
+sudo certbot --server https://acme-v02.api.letsencrypt.org/directory -d xtraorbitals.xyz -d *.xtraorbitals.xyz --manual --preferred-challenges dns-01 certonly
+
+# For renew
+#certbot renew
+
+
+# Get Jupyter examples
+
+# Get Nginx conf
+## sites-available && sites-enabled
+## Reload conf
+sudo systemctl reload nginx
