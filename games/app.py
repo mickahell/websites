0f20a4ceb40cc2c0dbd4329c29de73788546e93a
Mica <mika89700@gmail.com>
2021-06-21 20:25:31 +0200

    add game pages

---
+++ games/app.py
@@ -0,0 +1,26 @@
+import sys
+import streamlit as st
+from streamlit import cli as stcli
+
+st.set_page_config(page_title="Games", page_icon=":space_invader:", layout='wide', initial_sidebar_state='auto')
+
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
+header = """
+<div align="center">
+    <h1>Games</h1>
+</div>
+<br /><br />
+"""
+st.markdown(header, unsafe_allow_html=True)
+
+# Run Quantum Pokemon Fight
+if st.checkbox('Quantum Pokemon Fight'):
+    if __name__ == '__main__':
+        sys.argv = ["streamlit", "run", "quantum_pokemon-fight/game.py"]
+        sys.exit(stcli.main())
