1d041586b1055dec2b0ed664bdaf406e5b418cdb
Michaël <mika89700@gmail.com>
2021-06-24 13:50:26 +0200

    Update start.sh

--- games/qpokemon-fight/start.sh
+++ games/qpokemon-fight/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /opt/qpokemon/quantum_pokemon-fight
-ttyd -t titleFixed="QPokémon Fight" -t fontSize=30 python3 game.py
+ttyd -t titleFixed="QPokémon Fight" -t fontSize=25 python3 game.py
