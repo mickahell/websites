8ba79c51fc1a2a2500abe7519ac6b6630e9ff1d9
Mica <mika89700@gmail.com>
2021-07-15 21:13:56 +0200

    topbar

--- main/app.py
+++ main/app.py
@@ -2,18 +2,60 @@ import streamlit as st
 
 st.set_page_config(page_title="Xtra Orbitals", page_icon=":space_invader:", layout='wide', initial_sidebar_state='auto')
 
-hide_menu_style = """
+css = """
     <style>
         #MainMenu {visibility: hidden;}
         footer::before {content:'Xtra Orbitals™️ | Since 2021 | ';}
+        
+        .header{
+            background: #555;
+            color: #f1f1f1; 
+            position: fixed;
+            top: 0;} 
+            
+        .header {
+            overflow: hidden;
+            background-color: #333;}
+
+        .header a {
+            float: left;
+            color: #f2f2f2;
+            text-align: center;
+            padding: 10px 16px;
+            text-decoration: none;
+            font-size: 15px;}
+
+        .header a:hover {
+            background-color: #ddd;
+            color: black;}
+        
+        .header a.separateur {
+            background-color: #000;
+            color: white;}
+
+        .header a.active {
+            background-color: #FFF;
+            color: black;}
     </style>
 """
-st.markdown(hide_menu_style, unsafe_allow_html=True)
+st.markdown(css, unsafe_allow_html=True)
 
-header = """
+title = """
 <div align="center">
     <h1>Xtra Orbitals</h1>
 </div>
 <br /><br />
 """
-st.markdown(header, unsafe_allow_html=True)
+st.markdown(title, unsafe_allow_html=True)
+
+# Menu
+menu = """
+    <div class="header">
+        <a class="active" href="http://xtraorbitals.xyz"><b>Home</b></a>
+        <a href="http://quantum-lab.xtraorbitals.xyz"><b>Lab</b></a>
+        <a href="http://games.xtraorbitals.xyz"><b>Games</b></a>
+        <a class="separateur" <b>|</b></a>
+        <a href="http://about.xtraorbitals.xyz"><b>About</b></a>
+    </div>
+"""
+st.markdown(menu, unsafe_allow_html=True)
