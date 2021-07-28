f2046ed7b1f0e487be3c2361bb5ba72f95736195
Michaël <mika89700@gmail.com>
2021-07-28 14:09:24 +0200

    Update app.py

--- main/app.py
+++ main/app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="Xtra Orbitals", page_icon=":space_invader:", layout='wide', initial_sidebar_state='auto')
 
@@ -53,9 +55,43 @@ menu = """
 st.markdown(menu, unsafe_allow_html=True)
 
 title = """
-<div align="center">
-    <h1>Xtra Orbitals</h1>
-</div>
-<br /><br />
+    <div align="center">
+        <h1>Xtra Orbitals</h1>
+    </div>
+    <br /><br />
 """
 st.markdown(title, unsafe_allow_html=True)
+
+here = """
+    ## One platform to unify them all
+    This platform has as purpose to unify and make available all of my projects, so **Welcome** in my **webservices platform** !
+"""
+st.markdown(here)
+
+apps = """
+    <br /><br />
+    <h2>Apps available</h2>
+"""
+st.markdown(apps, unsafe_allow_html=True)
+col0, col1, col2, col3 = st.beta_columns(4)
+lab = col0.beta_expander("Quantum Lab", True)
+lab.write("```python3.8, networkx, numpy, matplotlib, notebook, pandas, scipy, tk```")
+games = col1.beta_expander("Games", True)
+games.write("```qiskit, qiskit[visualization], qiskit-nature```")
+fractals = col2.beta_expander("Fractals", True)
+fractals.write("```autograd, pennylane, pennylane-sf, pennylane-qiskit```")
+about = col3.beta_expander("About", True)
+about.write("```cirq, cirq-core[contrib], texlive-latex-base, latexmk```")
+
+## Tag cloud
+# Create some sample text
+text = 'Quantum, games, AI, robots, pokemon, lab, python, qiskit, pennylane, fractals, fun'
+
+# Create and generate a word cloud image:
+wordcloud = WordCloud().generate(text)
+
+# Display the generated image:
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+st.pyplot()
