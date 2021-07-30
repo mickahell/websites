9cbdebedafaf6ec93a8e0e0f1e38890ffb535ee1
Mica <mika89700@gmail.com>
2021-07-30 23:40:15 +0200

    main page format

--- main/app.py
+++ main/app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from wordcloud import WordCloud
+import streamlit.components.v1 as components
 import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="Xtra Orbitals", page_icon=":space_invader:", layout='wide', initial_sidebar_state='auto')
@@ -68,30 +68,29 @@ here = """
 """
 st.markdown(here)
 
-apps = """
-    <br /><br />
-    <h2>Apps available</h2>
-"""
-st.markdown(apps, unsafe_allow_html=True)
-col0, col1, col2, col3 = st.beta_columns(4)
-lab = col0.beta_expander("Quantum Lab", True)
-lab.write("```python3.8, networkx, numpy, matplotlib, notebook, pandas, scipy, tk```")
-games = col1.beta_expander("Games", True)
-games.write("```qiskit, qiskit[visualization], qiskit-nature```")
-fractals = col2.beta_expander("Fractals", True)
-fractals.write("```autograd, pennylane, pennylane-sf, pennylane-qiskit```")
-about = col3.beta_expander("About", True)
-about.write("```cirq, cirq-core[contrib], texlive-latex-base, latexmk```")
-
 ## Tag cloud
 # Create some sample text
-text = 'Quantum, games, AI, robots, pokemon, lab, python, qiskit, pennylane, fractals, fun'
+#text = 'Quantum, games, AI, robots, pokemon, lab, python, qiskit, pennylane, fractals, fun'
 
-# Create and generate a word cloud image:
-wordcloud = WordCloud().generate(text)
-
-# Display the generated image:
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis("off")
-plt.show()
-st.pyplot()
+components.html(
+"""
+<script src="https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js"></script>
+<script>mermaid.initialize({startOnLoad:true});</script>
+<br /><br />
+<div class="mermaid">
+%%{init: {'theme': 'base', 'themeVariables': {'primaryTextColor': '#FFF', 'primaryColor': '#000ccc', "fontSize": "20px"}}}%%
+journey
+            title Apps status
+            section Laboratories
+              Qiskit: 7: On
+              Pennylane: 7: On
+              Cirq: 7: On
+            section Games
+              qPokemon: 7: On
+              qNim: 7: On
+            section Fractals
+              Museum: 0: Building
+              auto-fractal: 0: Building
+            section qMaths
+</div>
+""", height=400, scrolling=False)
