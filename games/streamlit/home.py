81a3bd1832c95c439a635643ec8abc7611609b03
Mica <mika89700@gmail.com>
2021-08-11 18:58:08 +0200

    stat on home

--- games/streamlit/home.py
+++ games/streamlit/home.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import pandas as pd
+import datetime
+import plotly.graph_objects as go
 
 
 def app():
@@ -10,5 +13,67 @@ def app():
         I like created games and making then winnable by a machine. So here every of these games have a tough <b>robot as opponent !</b><br>
         Will you succeed to win each times ? Have fun and good luck !!! 
     </div>
+    </br>
     """
     st.markdown(home_page, unsafe_allow_html=True)
+
+    #############################################################
+    # Graph
+
+    robot_csv = []
+    human_csv = []
+    date_csv = []
+
+    ## Qpokemon
+    file_csv = 'stats/qpokemon_results.csv'
+    csv_file = pd.read_csv(file_csv, header=None)
+
+    for i in range(len(csv_file[0])):
+        robot_csv.append(csv_file[0][i])
+        human_csv.append(csv_file[1][i])
+        date_csv.append(datetime.datetime.strptime(csv_file[2][i], '%m-%Y').date())
+
+    ## Qnim
+    file_csv = 'stats/qnim_results.csv'
+    csv_file = pd.read_csv(file_csv, header=None)
+
+    ## Fusion
+    compteur = 0
+    for i in csv_file[2]:
+        temp_date = datetime.datetime.strptime(i, '%m-%Y').date()
+        if temp_date in date_csv:
+            compteur_bis = 0
+            for u in date_csv:
+                if u == temp_date:
+                    robot_csv[compteur_bis] += csv_file[0][compteur]
+                    human_csv[compteur_bis] += csv_file[1][compteur]
+                compteur_bis += 1
+        else:
+            robot_csv.append(csv_file[0][compteur])
+            human_csv.append(csv_file[1][compteur])
+            date_csv.append(temp_date)
+        compteur += 1
+
+    graph = """
+    ## Graph - Victory
+    """
+    st.markdown(graph)
+
+    fig_bar = go.Figure([go.Bar(x=['Robot', 'Human'], y=[sum(robot_csv), sum(human_csv)])])
+    fig_bar.update_layout(title_text='Robot VS Human')
+    st.plotly_chart(fig_bar, use_container_width=True)
+
+    left_column, right_column = st.beta_columns([1, 5])
+
+    graph_filter = left_column.radio("Filter", ("Robot", "Human"))
+    if graph_filter == 'Human':
+        graph_csv = human_csv
+    else:
+        graph_csv = robot_csv
+
+    fig_series = go.Figure([go.Scatter(x=date_csv, y=graph_csv, mode='lines+markers', marker=dict(color="aqua"))])
+    fig_series.update_layout(title_text='Robot evolution')
+    right_column.plotly_chart(fig_series, use_container_width=True)
+
+
+
