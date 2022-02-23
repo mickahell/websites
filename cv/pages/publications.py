d756acc4fe3d5e6b3e8e7b621a360dc7f2263a56
Michaël <mika89700@gmail.com>
2022-02-23 09:21:02 +0100

    Create publications.py

---
+++ cv/pages/publications.py
@@ -0,0 +1,39 @@
+import streamlit as st
+
+
+def publi(json: json):
+  """Show publications
+  Arg: 
+    json: json of the page publications
+  Return: nothing
+  """
+  st.write("## Publications")
+  for i in json["publications"]:
+    st.write("### " + i["title"])
+    st.write(i["author"])
+    st.write(i["domaine"])
+    st.write("Type of doc : " + i["type"] + " - Langue : " + i["language"])
+    st.write("DOI : " + i["doi"] + "- Hal id : " + i["hal_id"])
+    st.write("[Read it on Hal](" + i["hal_url"] + ")")
+    st.write(i["publication_date"])
+    with st.expander("Abstract :", False):
+      st.write(i["abstract"])
+
+      
+def blog(json: json):
+  """Show blogposts
+  Arg: 
+    json: json of the page blogposts
+  Return: nothing
+  """
+  st.write("## Blog posts")
+  for i in json["blogposts"]:
+    st.write("### " + i["title"])
+    st.write(i["author"])
+    st.write(i["domaine"])
+    st.write("Langue : " + i["language"])
+    st.write("DOI of the project : " + i["doi"])
+    st.write("[Read it](" + i["url"] + ")")
+    st.write(i["publication_date"])
+    with st.expander("Abstract :", False):
+      st.write(i["abstract"])
