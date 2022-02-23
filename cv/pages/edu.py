3b798f1f3aaf40644609dcbbc0fd846358969368
Michaël <mika89700@gmail.com>
2022-02-23 08:50:35 +0100

    Create edu.py

---
+++ cv/pages/edu.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+
+def edu(json: json, base_url: str):
+  """Show education
+  Arg: 
+    json: json of the page education
+  Return: nothing
+  """
+  st.write("## Education")
+  for i in json["education"]:
+    st.write("### " + i["diploma"])
+    level = i["level"] + " - " if i["level"] is not None else ""
+    st.write(level + i["date_begin"] + "  -> " + i["date_end"])
+    st.write(i["school"])
+    country_city = " - " + i["country"] + " " + i["city"] if i["country"] and i["city"] is not None else ""
+    
+def cert(json: json, base_url: str):
+  """Show certifications
+  Arg: 
+    json: json of the page certifications
+  Return: nothing
+  """
+  st.write("## Certifications")
+  for i in json["certifications"]:
+    st.write("### " + i["title"])
+    type_cert = "Hackaton" if i["diploma"] is not None else "Certification"
+    st.write(type_cert + " - " + i["date"])
+    st.write(i["organisme"])
+    st.write("Ref id : [" + i["ref_id"] +"](" + i["diploma_url"] + ")")
