9aff689ace22916779c0391b3f83c4ac5013107d
Michaël <mika89700@gmail.com>
2023-07-11 18:28:15 +0200

    Update start_jupyter.sh

--- quantum_lab/jupyter/start_jupyter.sh
+++ quantum_lab/jupyter/start_jupyter.sh
@@ -2,4 +2,4 @@
 
 cd /opt/quantum_lab/data/share/
 
-LANGUAGE="" LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8 jupyter notebook --allow-root --port=9999 --no-browser --ip='*' --NotebookApp.token=''
+jupyter notebook --allow-root --port=9999 --no-browser --ip='*' --NotebookApp.token=''
